2353e6a897ae063f542042a4909b0714b1f0f8b5
liangyuwu <yuwuliang@yeah.net>
2018-08-23 20:43:16 +0800

    synchronize

--- src/main/java/com/lyw/hadoop/sync_thread/LockThread.java
+++ src/main/java/com/lyw/hadoop/sync_thread/LockThread.java
@@ -3,6 +3,9 @@ package com.lyw.hadoop.sync_thread;
 import org.mortbay.util.ajax.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.objenesis.instantiator.util.UnsafeUtils;
+import sun.misc.Unsafe;
+import sun.reflect.Reflection;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,47 +24,55 @@ public class LockThread {
 
     final ReentrantLock lock = new ReentrantLock();
     Condition condition = lock.newCondition();
-//    private static final Unsafe unsafe = Unsafe.getUnsafe();
+    private static final Unsafe unsafe = UnsafeUtils.getUnsafe();
     static final Logger log = LoggerFactory.getLogger(LockThread.class);
+    static volatile int count = 0;
 
-    void getLockName(String lockName) {
+    void getLockName(String lockName) {//普通方式++ 模拟：线程不安全
 
         try {
-            log.error("锁编号1："+lock.toString());
-            lock.lock();
-            LockSupport.park();
-            log.info("锁编号2："+lock.toString());
-//            log.info("进入获取lock的方法,但未执行getLockName方法");
-            log.error("等待线程数："+ JSON.toString(lock)+"="+lock.getQueueLength());
-//            condition.await();
-//            Thread.sleep(60000);
+            log.error("操作前count："+count);
+
+            int a = count;
+            if ("lyw50".equals(lockName)) {
+                Thread.sleep(1000);
+            }
+            count = a + 1;
+            log.error("操作之后count："+ count);
         } catch (Exception e) {
 
         }finally {
-//            log.error("执行getLockName方法，打印name=" + lockName);
-            lock.unlock();
+
         }
 
-    }
-
-
-    public static void main2(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-        LockThread thread = new LockThread();
-        for (int i = 0; i < 1; i++) {
-            final int j = i;
-            executorService.submit(()->{
-                thread.getLockName("lyw"+j);
-            });
-        }
     }
 
 
     public static void main(String[] args) {
-        System.err.println("lock之前");
-        LockSupport.park();
-        System.err.println("第一次park已经完成，进行第二次");
-        System.err.println("o");
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        LockThread thread = new LockThread();
+        for (int i = 0; i < 500; i++) {
+            final int j = i;
+            Thread runnable = new Thread(()->{
+                thread.getLockName("lyw"+j);
+            });
+            runnable.start();
+//            executorService.submit(runnable);
+//            LockSupport.unpark(runnable);
+//            runnable.interrupt();
+
+//            System.err.println(runnable.getName()+"  不阻断主线程:"+runnable.isAlive());
+        }
+
+        new Thread(()->{
+            for(;;) {
+                if(count == 500){
+                    System.err.println(count);
+                    break;
+                }
+            }
+        }).start();
+
     }
 
 }
