9b93841fd4a5069495f2a84c0717c43327a6f1d3
liangyuwu <yuwuliang@yeah.net>
2018-08-29 20:02:56 +0800

    kmp字符串检索算法

--- src/main/java/com/lyw/hadoop/arithmetic/KMPArithmetic.java
+++ src/main/java/com/lyw/hadoop/arithmetic/KMPArithmetic.java
@@ -8,5 +8,52 @@ package com.lyw.hadoop.arithmetic;
  */
 public class KMPArithmetic {
 
+    char[] sub = {'a','b','c','d'};
+
+    int count;
+    int next;
+
+    public int indexOf(String chars) {
+
+        while (true) {
+            char[] s = getsubChar(chars,next);
+            lookup:
+            for (int i = 0; i < s.length; i++) {
+                for (int j = count; j < sub.length; j++) {
+                    if (count == i && s[i] == sub[j]) {
+                        count++;
+                        continue lookup;
+                    }
+                    count = 0;
+                    break lookup;
+                }
+            }
+
+            if (count == sub.length) {
+                return next-sub.length;
+            }
+
+        }
+    }
+
+    public char[] getsubChar(String s,int index) {
+        next = index + sub.length;
+        if (next > s.length()) {
+            return null;
+        }
+        String ss=s.substring(index, index + sub.length);
+        return ss.toCharArray();
+    }
+
+    public int next(int j) {
+        return j;
+    }
+
+    public static void main(String[] args) {
+        KMPArithmetic k = new KMPArithmetic();
+        char[] chars = "eafdadfasdfsaefasfesabcd".toCharArray();
+        int i =  k.indexOf("eafdadfasdfsaefasfesssssssssssabcd");
+        System.err.println("index="+i);
+    }
 
 }
