f705fb3c689b60e84eaa8415c7b9a18d32087a94
Keepontrying <yuwuliang@yeah.net>
2018-08-30 08:20:35 +0800

    synchronized

--- src/main/java/com/lyw/hadoop/java_gc/ReferenceTest.java
+++ src/main/java/com/lyw/hadoop/java_gc/ReferenceTest.java
@@ -5,4 +5,12 @@ package com.lyw.hadoop.java_gc;
  */
 public class ReferenceTest {
 
+    public static void main(String[] args) {
+        System.gc();
+        {
+            byte[] a = new byte[1024 * 1024 * 8];
+        }
+//        int b =0;
+        System.gc();
+    }
 }
