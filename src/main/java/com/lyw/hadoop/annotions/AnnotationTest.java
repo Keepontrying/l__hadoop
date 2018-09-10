13cd9bb2f226e2233324a571d6cb763c5a11a8e2
liangyuwu <yuwuliang@yeah.net>
2018-09-10 19:49:45 +0800

    kmp字符串检索算法

---
+++ src/main/java/com/lyw/hadoop/annotions/AnnotationTest.java
@@ -0,0 +1,17 @@
+package com.lyw.hadoop.annotions;
+
+import java.lang.annotation.*;
+
+/**
+ * 类描述：注解学习
+ *
+ * @author liangyuwu
+ * @Time 2018/9/10 9:42
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface AnnotationTest {
+
+
+}
